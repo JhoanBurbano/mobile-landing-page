3b33bfa41c82f8830251717211ce7495e9a18006
jsburbano <joans.burbano@gmail.com>
2024-11-25 03:04:34 -0500

    feat: init portfolio template

---
+++ src/components/Projects.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
+const Projects = () => {
+  const [currentTab, setCurrentTab] = useState<number>(0);
+  const tabs = ['Mobile', 'Web', 'Backend'];
+  useEffect(() => {
+    console.log('Tab changed to:', tabs[currentTab]);
+  }, [currentTab]);
+  const handleCurrentTab = (index: number) => {
+    setCurrentTab(index);
+  };
+  const __renderTabContent = useCallback(() => {
+    switch (currentTab) {
+      case 0:
+        return (
+          <React.Fragment>
+            <section>
+              <h3>Tasker</h3>
+              <p>
+                This is a project to do task with theme dark and light switch
+                and ui ux paradigms and async storage use.
+              </p>
+            </section>
+            <aside>
+              <div
+                data-snack-id="@jsburbano/github.com-jhoanburbano-tc-survely-app"
+                data-snack-platform="android"
+                data-snack-preview="true"
+                data-snack-theme="dark"
+                data-snack-supportedplatforms="mydevice,ios,android"
+                className="rounded-xl bg-gray-700 overflow-hidden border-2 border-gray-800 w-full h-[720px]"
+              ></div>
+            </aside>
+          </React.Fragment>
+        );
+      case 1:
+        return (
+          <React.Fragment>
+            <section>
+              <h3>Tasker</h3>
+              <p>
+                This is a project to do task with theme dark and light switch
+                and ui ux paradigms and async storage use.
+              </p>
+            </section>
+            <aside>
+              <iframe
+                src="https://social-insights.vercel.app/"
+                ancestor-origins=""
+                style={{
+                  width: '100%',
+                  height: '720px',
+                }}
+              ></iframe>
+            </aside>
+          </React.Fragment>
+        );
+      case 2:
+        return <div>Backend Projects</div>;
+      default:
+        return <div>Mobile Projects</div>;
+    }
+  }, [currentTab]);
+  return (
+    <main>
+      <ul className="flex gap-2">
+        {tabs.map((t, i) => {
+          let styles =
+            'p-2 rounded-lg shadow-[#00000055] shadow-sm text-xs font-bold cursor-pointer';
+          if (currentTab === i) {
+            styles +=
+              ' hover:bg-sky-200 active:bg-sky-400 text-gray-900  bg-sky-300';
+          } else {
+            styles +=
+              ' hover:bg-zinc-200 active:bg-zinc-400 text-gray-900  bg-zinc-300';
+          }
+          return (
+            <li key={i}>
+              <button onClick={() => handleCurrentTab(i)} className={styles}>
+                {t}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {__renderTabContent()}
+      <script async src="https://snack.expo.dev/embed.js"></script>
+    </main>
+  );
+};
+
+export default Projects;
---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,23 @@
+export const SEQUENCE_COLORS_TW = [
+  'text-green-600',
+  'text-red-600',
+  'text-yellow-600',
+  'text-blue-600',
+  'text-indigo-600',
+  'text-purple-600',
+  'text-pink-600',
+];
+
+export const SEQUENCE_BLUE_TW = [
+  'text-emerald-500',
+  'text-teal-500',
+  'text-cyan-500',
+  'text-blue-400',
+  'text-sky-500',
+];
+
+export const SEQUENCE_RED_TW = [
+  'text-pink-600',
+  'text-rose-600',
+  'text-red-600',
+];
